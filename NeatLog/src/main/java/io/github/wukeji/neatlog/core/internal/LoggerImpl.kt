f962864d3137f6a36e812edd4889692f6b4fde43
yangxiaobin01 <yangxiaobin01@zuoyebang.com>
2025-12-09 21:43:23 +0800

    Fix NPE.

--- NeatLog/src/main/java/io/github/wukeji/neatlog/core/internal/LoggerImpl.kt
+++ NeatLog/src/main/java/io/github/wukeji/neatlog/core/internal/LoggerImpl.kt
@@ -5,13 +5,15 @@ import io.github.wukeji.neatlog.core.LogFacade
 import io.github.wukeji.neatlog.domain.DomainContext
 import io.github.wukeji.neatlog.domain.EmptyDomainContext
 
-internal class LoggerImpl(private val extraCtx: DomainContext = EmptyDomainContext) :
-    AbsLogger() {
+internal class LoggerImpl(
+    private val extraCtx: DomainContext = EmptyDomainContext
+) : AbsLogger() {
+
     override val logContext: DomainContext get() = super.logContext + extraCtx
 
     override fun clone(newLogContext: DomainContext?): LogFacade {
-        val moreCtx = logContext + (newLogContext ?: EmptyDomainContext)
-        return LoggerImpl(extraCtx + moreCtx)
+        val merged = extraCtx + (newLogContext ?: EmptyDomainContext)
+        return LoggerImpl(merged)
     }
 
     override fun dumpContext(): String = logContext.dump()
